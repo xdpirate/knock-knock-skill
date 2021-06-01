0d4b497d819272acc00da967701146056d080ecc
xdpirate <1757462+xdpirate@users.noreply.github.com>
2021-06-02 00:36:28 +0200

    Make it work

--- __init__.py
+++ __init__.py
@@ -1,15 +1,15 @@
 from mycroft import MycroftSkill, intent_file_handler
 
-
 class KnockKnock(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
     @intent_file_handler('knock.knock.intent')
     def handle_knock_knock(self, message):
-        self.speak_dialog('knock.knock')
+        who = self.get_response('knock.knock')
+        punchline = self.get_response(who + ' who?')
+        self.speak_dialog('gratification')
 
-
+    
 def create_skill():
     return KnockKnock()
-
